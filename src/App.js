a9d28fe3675effeeac0e18482213bb9e0450c076
harshmpatel1 <patelharsh7458@gmail.com>
2021-10-10 15:00:06 +0530

    simple sample of site without database

---
+++ src/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import './App.css';
+import Card from './Card';
+
+function App() {
+  return (
+    <div>
+    <h1>Hallow Your facts are here</h1><hr />
+    <Card info=" india having most young population"  />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+const Card = (props)=>{
+  return(
+    <>
+
+    <h1> {props.info}</h1>
+    
+    </>
+  )
+}
+
+export default Card
