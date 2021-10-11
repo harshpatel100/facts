64572db6bfd753846742b92fa55f3a92d99fa6e3
harshmpatel1 <patelharsh7458@gmail.com>
2021-10-11 11:27:28 +0530

    styling card

--- src/Card.jsx
+++ src/Card.jsx
@@ -6,12 +6,13 @@ const Card = ()=>{
 
     {  facts.map((fact,index)=>{
         return(
-          <>
+          <div className="card">
+           <div className="card-image"></div>
+          <div className="card-text">
           <h1>Fact No :{index} </h1>
-          <div classname="semi-card">
           <h2>{fact}</h2>
           </div>
-          </>
+          </div>
         )
     })   }
     
